a9429e074b9e41e6acf64c6367c7620cf7f2fb6c
Shahed Ahmad <shaheda.a16@gmail.com>
2021-12-09 22:00:32 +0430

    components for accessing data created and hook for simple dispatches but app not configured in index.tsx

---
+++ src/components/App.tsx
@@ -0,0 +1,13 @@
+import { Provider } from "react-redux";
+import { store } from "../redux";
+
+const App = () => {
+    return <Provider store={store}>
+        <div>
+            <h1>Search For A Package</h1>
+            
+        </div>
+    </Provider>
+}
+
+export default App;
